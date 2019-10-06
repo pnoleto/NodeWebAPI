d7e35249b2429214502ad5c11867165330101d62
PhelipeNoleto <phelipe.noleto@outlook.com>
2019-10-06 18:24:56 -0300

    Ajustes de retorno nas funcoes de autenticação

--- services/authenticate.service.js
+++ services/authenticate.service.js
@@ -6,26 +6,26 @@ const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', l
 
 async function authenticate({ username, password }) {
     const user = users.find(u => u.username === username && u.password === password);
+
     if (user) {
         const { password, ...userWithoutPassword } = user;
         const token = jwt.sign({ id: user.id, name: username }, config.secret, { expiresIn: config.expiresIn, algorithm: config.algorithm });
         const refreshToken = jwt.sign(user, config.refreshTokenSecret, { expiresIn: config.refreshTokenExpiresIn, algorithm: config.refreshTokenAlgorithm });
         return { ...userWithoutPassword, token, refreshToken };
     }
-    throw { name:'InvalidCredential', message: 'Username or password is incorrect' };
+    throw { name: 'InvalidCredential', message: 'Username or password is incorrect' };
 }
 
 async function refreshToken({ refreshToken }) {
-    try {
-        const decodedPlayload = jwt.verify(refreshToken, config.refreshTokenSecret);
-        if (decodedPlayload) {
-            const { password, ...userWithoutPassword } = decodedPlayload;
-            const token = jwt.sign({ decodedPlayload }, config.secret, { expiresIn: config.expiresIn, algorithm: config.algorithm });
-            return { ...userWithoutPassword, token };
-        }
-    } catch (err) {
-        throw err;
+    const decodedPlayload = jwt.verify(refreshToken, config.refreshTokenSecret);
+
+    if (decodedPlayload) {
+        const { password, ...userWithoutPassword } = decodedPlayload;
+        const token = jwt.sign({ decodedPlayload }, config.secret, { expiresIn: config.expiresIn, algorithm: config.algorithm });
+        return { ...userWithoutPassword, token };
     }
+
+    throw err;
 }
 
 module.exports = { authenticate, refreshToken };
