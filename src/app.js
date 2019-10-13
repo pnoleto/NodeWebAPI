509071acfdea225176fb9424c01a19186003927a
PhelipeNoleto <phelipe.noleto@outlook.com>
2019-10-12 22:01:03 -0300

    Separação do log em uma service

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const jwtMiddleWare = require('./../helpers/jwtMiddleware');
 const authRoute = require('./routes/authenticateRoute');
 const errorHandler = require('../helpers/errorHandler');
+const logger = require('../services/logger.service'); 
 const personRoute = require('./routes/personRoute');
 const { corsOptions } = require('./../config.json');
-const expressWinston = require('express-winston');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
-const winston = require('winston');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -25,16 +24,7 @@ app.use(`/v1`, index);
 app.use(`/v1/persons`, personRoute);
 app.use(`/v1/users`, authRoute);
 
-app.use(expressWinston.errorLogger({
-    transports: [
-        new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-        winston.format.prettyPrint(),
-        winston.format.timestamp(),
-        winston.format.colorize(),
-    )
-}));
+app.use(logger);
 
 app.use(errorHandler);
 
