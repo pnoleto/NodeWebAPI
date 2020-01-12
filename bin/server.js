1ceff90228e7f417af3535e6eb23c89af4e9b07f
PhelipeNoleto <phelipe.noleto@outlook.com>
2020-01-12 17:39:50 -0300

    package.json
    Correcao no script de start do servidor

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 const port = normalizePort(process.env.PORT || 3000);
 
-function normalizePort(val) {
+async function normalizePort(val) {
     const port = parseInt(val, 10);
 
     if (!isNaN(port) && port >= 0)
