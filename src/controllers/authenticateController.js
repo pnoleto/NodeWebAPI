23ecd19f48643678ef0fb7814195a04f30cc8997
PhelipeNoleto <phelipe.noleto@outlook.com>
2020-05-13 22:45:01 -0300

    *Ajustes nas funções assincronas de autenticação

--- src/controllers/authenticateController.js
+++ src/controllers/authenticateController.js
@@ -1,17 +1,19 @@
 const loginService = require('./../../services/authenticate.service');
 
-exports.authenticate = (req, res, next) => {
-    loginService.authenticate(req.body)
-        .then((token) =>
-            res.status(200).json(token))
-        .catch((err) =>
-            next(err));
+exports.authenticate = async (req, res, next) => {
+    try {
+        var token = await loginService.authenticate(req.body);
+        res.status(200).json(token);
+    } catch (error) {
+        next(error);
+    }
 };
 
-exports.refreshToken = (req, res, next) => {
-    loginService.refreshToken(req.body)
-        .then((refreshToken) =>
-            res.status(200).json(refreshToken))
-        .catch((err) =>
-            next(err));
+exports.refreshToken = async (req, res, next) => {
+    try {
+        const refreshToken = await loginService.refreshToken(req.body);
+        res.status(200).json(refreshToken);
+    } catch (error) {
+        next(error)
+    }
 };
